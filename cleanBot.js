8746bfd05eb621ece83d82672a6ffadc007c3714
yvoytovych <yuriy.voytovych@hotschedules.com>
2019-09-25 17:59:58 +0300

    make use of bot username

--- cleanBot.js
+++ cleanBot.js
@@ -3,14 +3,18 @@ const Telegram = require('telegraf/telegram');
 const telegram = new Telegram('919776689:AAGFl5AewtyQ2mZ7Li7bSOEZVt0fVNkoL9k');
 const bot = new Telegraf('919776689:AAGFl5AewtyQ2mZ7Li7bSOEZVt0fVNkoL9k');
 
-const welcome = "Нарешті, я буду накуй видаляти кожне повідомлення, не пов'язане з музикою\n" +
-    "Лишу тільки soundcloud i youtube music\n" +
-    "Інтервал - 1 хвилина, час пішов :)";
-bot.start(ctx => ctx.reply(welcome));
+const welcome = "Видаляю всі повідомлення не пов'язані з музикою\n" +
+    "Лишу тільки soundcloud\n" +
+    "Інтервал - 1 хвилина";
+
+bot.telegram.getMe().then((botInfo) => {
+    bot.options.username = botInfo.username;
+});
+
+bot.command('help',ctx => ctx.reply(welcome));
 
 const DELETE_AFTER = 60 * 1000;
 const MUSIC_PATTERN = /^http.*(soundcloud|youtube)\.com(\/[\d\w-?=&]+)+/;
-
 bot.on('message', (ctx) => {
     const { message_id, text = '', chat } = ctx.update.message;
     console.log(text, message_id, chat.id);
@@ -18,4 +22,4 @@ bot.on('message', (ctx) => {
         setTimeout(() => telegram.deleteMessage(chat.id, message_id), DELETE_AFTER);
 });
 
-bot.startPolling();
+bot.launch();
